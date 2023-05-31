be86c9acf3edf85a7d8bf431e7f16e5c05235ca6
Josh Kelly <josh@lovingfamily.co.uk>
2023-05-31 16:59:57 +0100

    --- Auto Git Commit ---

--- account_admin.php
+++ account_admin.php
@@ -1,5 +1,8 @@
 <?php
-if ()
+session_start();
+if ($_SESSION['admin'] == false) {
+    header("Location:login.php");
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,7 +17,31 @@ if ()
 </head>
 
 <body>
-
+    <?php
+    include "inc/header.php";
+    ?>
+    <div id="container">
+        <table>
+            <tr>
+                <th>Order Number</th>
+                <th>Order Date</th>
+                <th>Order Status</th>
+                <th>Order Details</th>
+                <th>Postage Details</th>
+            </tr>
+            <?php
+            require "src/database.php";
+            $sql = "SELECT * FROM orders INNER JOIN customers ON orders.customer_id=customers.customer_id WHERE complete = 1";
+            $sqlQuery = mysqli_query($db, $sql);
+            foreach (mysqli_fetch_assoc($sqlQuery) as $user) {
+                echo 
+            }
+            ?>
+        </table>
+    </div>
+    <?php
+    include "inc/footer.php";
+    ?>
 </body>
 
 </html>
