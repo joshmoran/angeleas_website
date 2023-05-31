e7d6c1ac4bb08173ad9a0085060d385bcaa3a915
Josh Kelly <josh@lovingfamily.co.uk>
2023-05-31 19:13:33 +0100

    --- Auto Git Commit ---

--- account_admin.php
+++ account_admin.php
@@ -14,65 +14,78 @@ if ($_SESSION['admin'] == false) {
     <meta name="author" content="">
     <link rel="stylesheet" href="src/css/css.css" type="text/css" />
     <title></title>
+    <style>
+        table,
+        td,
+        th,
+        tr {
+            border: 1px solid #000;
+        }
+
+        th {
+            width: 5em;
+        }
+
+        td {
+            width: 10em;
+        }
+    </style>
+
 </head>
 
-<body>
-    <?php
-    include "inc/header.php";
-    ?>
-    <div id="container">
-        <table>
-            <tead>
-                <th>Order Number</th>
+<body><?php
+        include "inc/header.php";
+        ?><div id="container">
+        <table><?php
+                require "src/database.php";
+                $sql = "SELECT * FROM orders LEFT JOIN customers ON orders.customer_id=customers.customer_id WHERE complete = 1";
+                $sqlQuery = mysqli_query($db, $sql);
+                $string = '<thead><th>Order Number</th>
                 <th>Order Date</th>
                 <th>Order Status</th>
                 <th>Order Details</th>
                 <th>Postage Details</th>
-                </thead>
-                <tbody>
-                    <?php
-                    require "src/database.php";
-                    $sql = "SELECT * FROM orders LEFT JOIN customers ON orders.customer_id=customers.customer_id WHERE complete = 1";
-                    $sqlQuery = mysqli_query($db, $sql);
-                    $string = '';
-                    while ($user = $sqlQuery->fetch_assoc()) {
+                <th>Accept Order</th>
+                <th>Order Dispatched</th>
+                </thead>';
+                while ($user = $sqlQuery->fetch_assoc()) {
 
 
-                        $string .= '<tr';
+                    $string .= '<tr>';
 
-
-                        $string .=  '<td>' . $user['order_id'] . '</td';
-                        $string .=  '<td>' . $user['time_ordered'] . '</td>';
-                        $string .=  '<td>' . $user['status'] . '</td>';
-                        // Customer ID
-                        $customerID = $_SESSION['customer_id'];
-                        // Customer Order Details
-                        $sqlItems = "SELECT purchases.quantity, purchases.product_id, products.name FROM purchases INNER JOIN products ON purchases.product_id=products.id WHERE purchases.customer_id = " . $customerID;
-                        $sqlQueryItems = mysqli_query($db, $sqlItems);
-                        $string .=  '<td>';
-                        while ($item = $sqlQueryItems->fetch_assoc()) {
-                            $string .=  $item['name'] . ' X ' . $item['quantity'] . '<br>';
-                        }
-                        $string .=  '</td>';
-                        // Customer Address
-                        $address = explode(",", $user['addres']);
-                        $string .=  '<td>' . $user['name'] . '<br>' . trim($address[0]) . '<br>' . trim($address[1]) . '<br>' . trim($address[2]) . '<br>' . trim($address[3]) . '<br>' . trim($address[4]) . '</td>';
-                        // $sqlAddress = "SELECT * FROM address WHERE customer_id = $customerID";
-                        // $sqlQueryAddress = mysqli_query($db, $sqlAddress);
-                        // foreach (mysqli_fetch_assoc($sqlQueryAddress) as $address) {
-                        //     
-                        // }
-                        $string .=  '</tr>';
+                    var_dump($user);
+                    $string .=  '<td>' . $user['order_id'] . '</td>';
+                    $string .=  '<td>' . $user['time_ordered'] . '</td>';
+                    $string .=  '<td>' . $user['status'] . '</td>';
+                    // Customer ID
+                    $customerID = $_SESSION['customer_id'];
+                    // Customer Order Details
+                    $sqlItems = "SELECT * FROM purchases INNER JOIN products ON purchases.product_id=products.id WHERE purchases.customer_id = " . $customerID;
+                    $sqlQueryItems = mysqli_query($db, $sqlItems);
+                    $string .=  '<td><ul>';
+                    while ($item = $sqlQueryItems->fetch_assoc()) {
+                        $string .=  '<li>' . $item['name'] . ' X ' . $item['quantity'] . '</li>';
                     }
+                    $string .=  '</ul></td>';
+                    // Customer Address
+                    $address = explode(",", $user['addres']);
+                    $string .=  '<td>' . $user['name'] . '<br>' . trim($address[0]) . '<br>' . trim($address[1]) . '<br>' . trim($address[2]) . '<br>' . trim($address[3]) . '<br>' . trim($address[4]) . '</td>';
+                    // $sqlAddress = "SELECT * FROM address WHERE customer_id = $customerID";
+                    // $sqlQueryAddress = mysqli_query($db, $sqlAddress);
+                    // foreach (mysqli_fetch_assoc($sqlQueryAddress) as $address) {
+                    //     
+                    // }
+                    $string .= '<td><a href="account_admin.php?accept=true&id=' . $user['order_id'] . '"><button>Accept</button></a></td>';
 
-                    echo $string;
-                    ?>
-                <tbody>
+                    $string .= '<td><a href="account_admin.php?accept=true&id=' . $user['order_id'] . '"><button>Accept</button></a></td>';
+                    $string .=  '</tr>';
+                }
+
+                echo $string;
+                ?><tbody>
         </table>
-    </div>
-    <?php
-    include "inc/footer.php";
-    ?>
-</body>
+    </div><?php
+            include "inc/footer.php";
+            ?></body>
 
 </html>
