cc7eef2848575b70ac84857c9eeb37f5abbfc61b
Josh Kelly <josh@lovingfamily.co.uk>
2023-06-26 20:39:58 +0100

    --- Auto Git Commit ---

--- account.php
+++ account.php
@@ -1,15 +1,17 @@
 <?php
 session_start();
 require 'src/variables.php';
-if ($_SESSION['loggedIn'] == false) {
+if ($_SESSION['loggedIn'] == false || !isset($_SESSION['loggedIn'])) {
     header("Location: login.php");
 }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
     <meta charset="utf-8">
+    <meta name="description" content="Main Page for logged in accounts and offers use for: viewing orders or changing details">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $websiteName; ?> - Account Home</title>
     <link href="src/css/css.css" rel="stylesheet" type="text/css" />
     <link href="src/css/account.css" rel="stylesheet" type="text/css" />
