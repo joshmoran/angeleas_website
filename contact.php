f0f5a23434f74c9a26c79c580d9ed10c9c30f8cb
Josh Kelly <josh@lovingfamily.co.uk>
2023-07-04 16:10:09 +0100

    VSCode Auto Git Commit

--- contact.php
+++ contact.php
@@ -4,24 +4,38 @@ require "src/variables.php";
 
 if (isset($_POST['email'])) {
 	$to = 'josh@lovingfamily.co.uk';
-	
-	if ( !empty($_POST))$subject = $_POST['subject'];
-
-	$message = "Name: " . $_POST['name'] . "<br>Customer Number: ";
-	if (isset($_SESSION['customer_id '])) {
-		$message .= $_SESSION['customer_id'];
+	$errors = 0;
+	if (!empty($_POST['subject'])) {
+		$subject = $_POST['subject'];
 	} else {
-		$message .= ' guest';
+		$errors++;
+		$errorSubject = "Subject is required";
 	}
-	$message .= "<br>Message: " . $_POST['message'];
-	// Always set content-type when sending HTML email
+
+	if (empty($_POST['name'])) {
+		$errors++;
+		$errorName = "Name is required";
+	}
+
+	if (!empty($_POST['message'])) {
+		$message = "Name: " . $_POST['name'] . "<br>Customer Number: ";
+		if (isset($_SESSION['customer_id '])) {
+			$message .= $_SESSION['customer_id'];
+		} else {
+			$message .= ' guest';
+		}
+		$message .= "<br>Message: " . $_POST['message'];
+		// Always set content-type when sending HTML email
+	} else {
+		$errorMessage = 'A message is required.';
+	}
 	$headers = "MIME-Version: 1.0" . "\r\n";
 	$headers .= "Content-type:text/html;" . "charset=iso-8859-1" . "\r\n";
 
 	// More headers
 	$headers .= 'From: <webmaster@lovingfamily.co.uk>' . "\r\n";
 	//$headers .= 'Cc: myboss@example.com' . "\r\n";
-	if ( count($errors) == 0 ) {
+	if ($errors == 0) {
 		if (mail($to, $subject, $message, $headers)) {
 			$error_message = 'Message sent successfully.';
 		} else {
@@ -60,24 +74,54 @@ if (isset($_POST['email'])) {
 			?>
 		</div>
 		<form action="contact.php" id="email" method="post">
-			<h2>Contact Us</h1>
-				<p>Please contact us for any queries, issues or questions that you want answered.</p>
-				<p>Please allow for 72 hours for a response</p>
+			<tr>
+				<th colspan="3">
+					<h2>Contact Us</h2>
+				</th>
+			</tr>
+			<tr>
+				<th colspan="3">
+					<p>Please contact us for any queries, issues or questions that you want answered.</p>
+				</th>
+			</tr>
 
-				<label for="name">Your Name: </label>
-				<input type="text" name="name" id="name" placeholder="John Smith">
+			<tr>
+				<th colspan="3">
+					<p>Please allow for 72 hours for a response</p>
+				</th>
+			</tr>
+			<tr>
+				<td><label for="name">Your Name: </label></td>
+				<td><input type="text" name="name" id="name" placeholder="John Smith"></td>
+				<td>
+					<p><?php if (isset($errorName)) echo $errorName; ?></p>
+				</td>
+			</tr>
+			<tr>
+				<td><label for="subject">Subject: </label></td>
+				<td><input type="text" name="subject" id="subject" placeholder="Reason for contacting"></td>
+				<td>
+					<p class="error"><?php if (isset($errorSubject)) echo $errorSubject; ?></p>
+				</td>
+			</tr>
+			<tr>
+				<td><label for="message">Message: </label></td>
+				<td><textarea name="message" id="message" cols="40" rows="8">Write Something</textarea></td>
+				<td>
+					<p class="error"><?php if (isset($errorMessage)) {
+											echo $errorMessage;
+										} ?></p>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="3"><input type="hidden" name="customerid" value="'<?php if (isset($_SESSION['loggedIn'])) {
+																					echo $_SESSION['customer_id'];
+																				} ?>" ?></td>
+			</tr>
 
-				<label for="subject">Subject: </label>
-				<input type="text" name="subject" id="subject" placeholder="Reason for contacting">
-
-				<label for="message">Message: </label>
-				<textarea name="message" id="message" cols="40" rows="8">Write Something</textarea>
-
-				<input type="hidden" name="customerid" value="'<?php if (isset($_SESSION['loggedIn'])) {
-																	echo $_SESSION['customer_id'];
-																} ?>" ?>
-
-				<input type="submit" name="email" value="Submit">
+			<tr>
+				<td colspan="3"><input type="submit" name="email" value="Submit"></td>
+			</tr>
 		</form>
 	</div>
 
